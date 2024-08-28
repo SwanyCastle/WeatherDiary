4c1e931d889725c0e9583eefe3f8188116bd2ddb
SwanyCastle <seunghwan712@gmail.com>
2024-08-28 12:47:45 +0900

    ✨ [Feat] Transaction 처리 및 OpenWeatherMap API Schedule 처리
    DiaryService 클래서에서 CRUD 기능에 Transaction 처리
    OpenWeatherMap API 의 날씨 정보를 지정된 시간에 DB 에 저장하는 Schedule 처리 후
    기존 Diary 생성시 OpenWeatherMap API 를 직접 호출해 데이터 저장하지 않고
    DB 에 저장된 데이터중 해당하는 날짜의 데이터를 가져와서 Diary 생성

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -6,8 +6,12 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.DateWeather;
 import zerobase.weather.domain.Diary;
+import zerobase.weather.repository.DateWeatherRepository;
 import zerobase.weather.repository.DiaryRepository;
 
 import java.io.BufferedReader;
@@ -24,39 +28,77 @@ import java.util.Map;
 public class DiaryService {
 
     private final DiaryRepository diaryRepository;
+    private final DateWeatherRepository dateWeatherRepository;
 
     @Value("${openweathermap.key}")
     private String apiKey;
 
+    @Transactional()
     public void createDiary(LocalDate date, String text) {
-        String weatherData = getWeather();
-        Map<String, Object> parsedWeather = parseWeather(weatherData);
-        Diary nowDiary = Diary.builder()
-                .weather(parsedWeather.get("main").toString())
-                .icon(parsedWeather.get("icon").toString())
-                .temperature((Double) parsedWeather.get("temp"))
-                .text(text)
-                .date(date)
-                .build();
+        DateWeather weatherData = getWeatherFormDB(date);
+
+//        Diary nowDiary = Diary.builder()
+//                .weather(weatherData.getWeather())
+//                .icon(weatherData.getIcon())
+//                .temperature(weatherData.getTemperature())
+//                .text(text)
+//                .date(date)
+//                .build();
+
+        Diary nowDiary = new Diary();
+        nowDiary.setDateWeather(weatherData);
+        nowDiary.setText(text);
+
         diaryRepository.save(nowDiary);
     }
 
+    @Transactional(readOnly = true)
     public List<Diary> readDiary(LocalDate date) {
         return diaryRepository.findAllByDate(date);
     }
 
+    @Transactional(readOnly = true)
     public List<Diary> readDiaries(LocalDate startDate, LocalDate endDate) {
         return diaryRepository.findAllByDateBetween(startDate, endDate);
     }
 
+    @Transactional
     public void updateDiary(LocalDate date, String text) {
         Diary nowDiary = diaryRepository.getFirstByDate(date);
         nowDiary.setText(text);
         diaryRepository.save(nowDiary);
     }
 
+    @Transactional
     public void deleteDiary(LocalDate date) {
         diaryRepository.deleteAllByDate(date);
+    }
+
+    @Transactional
+//    @Scheduled(cron = "0/3 * * * * *") // 3초마다 데이터 불러오는지 확인 - 날짜 값이 기본키라 한 개 밖에 저장 안됨
+    @Scheduled(cron = "0 0 1 * * *")
+    public void saveWeatherDate() {
+        dateWeatherRepository.save(getWeatherFromApi());
+    }
+
+    private DateWeather getWeatherFromApi() {
+        String weatherData = getWeather();
+        Map<String, Object> parsedWeather = parseWeather(weatherData);
+        return DateWeather.builder()
+                .date(LocalDate.now())
+                .weather(parsedWeather.get("main").toString())
+                .icon(parsedWeather.get("icon").toString())
+                .temperature((Double) parsedWeather.get("temp"))
+                .build();
+    }
+
+    private DateWeather getWeatherFormDB(LocalDate date) {
+        List<DateWeather> weatherList = dateWeatherRepository.findAllByDate(date);
+
+        if (weatherList.isEmpty()) {
+            return getWeatherFromApi();
+        }
+        return weatherList.get(0);
     }
 
     private String getWeather() {
