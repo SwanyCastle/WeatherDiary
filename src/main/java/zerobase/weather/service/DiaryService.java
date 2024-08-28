9e4977edfc093bdb22034cd91e0011cd9e4eb1c8
SwanyCastle <seunghwan712@gmail.com>
2024-08-28 14:49:35 +0900

    ✨ [Feat] Exception Handler 추가
    config 폴더에 GlobalExceptionHandler 를 만들어서
    Controller 에서 오는 Exception 들을 처리

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import zerobase.weather.WeatherApplication;
 import zerobase.weather.domain.DateWeather;
 import zerobase.weather.domain.Diary;
+import zerobase.weather.exception.InvalidDateException;
 import zerobase.weather.repository.DateWeatherRepository;
 import zerobase.weather.repository.DiaryRepository;
 
@@ -62,6 +63,9 @@ public class DiaryService {
     @Transactional(readOnly = true)
     public List<Diary> readDiary(LocalDate date) {
         logger.info("Read diary");
+//        if (date.isAfter(LocalDate.ofYearDay(3050, 1))) {
+//            throw new InvalidDateException();
+//        }
         return diaryRepository.findAllByDate(date);
     }
 
